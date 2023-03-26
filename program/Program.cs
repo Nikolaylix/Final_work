a661c1dc51f91258546f9b8616402782e4c56fd8
Nikolay <sayoker6@gmail.com>
2023-03-26 23:06:28 +0800

    Написал два блока

--- program/Program.cs
+++ program/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿System.Console.Write("Enter string: ");
+string? str = Console.ReadLine();
+
+// Делим введённые данные на массив строк.
+char[] delimeters = { ' ', ',', '.' };
+string[]? strArray = str?.Split(delimeters, StringSplitOptions.RemoveEmptyEntries);
+
+// Выводим массив и сразу считаем количество искомых элементов.
+int count = 0;
+for (int i = 0; i < strArray?.Length; i++)
+{
+    if (strArray[i].Length <= 3)
+        count++;
+    System.Console.WriteLine($"strArray[{i}] = " + strArray[i]);
+}
+System.Console.WriteLine();
+
